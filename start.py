195f91e0451effea29df1d8271a473d3d947e3a8
skycrush777 <skrajewski@zut.edu.pl>
2023-10-09 12:37:26 +0200

    Pliki do uaktualnienia 12:32

--- start.py
+++ start.py
@@ -1,9 +1,9 @@
-print("Hello wor ld")
+print("Hello world")
 rok = 2023
 print(type(rok))
 imie = "Slaawko"
 print(type(imie))
-temp = 36.614
+temp = 36.6
 print(type(temp))
 czy_jest_zimno = True
 # true znaczy że jest 1, natomiast jak jest false to znaczy że jest 0
