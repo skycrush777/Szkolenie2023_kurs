9dee12b766136a17a98649ae0b5f793921451cde
skycrush777 <skrajewski@zut.edu.pl>
2023-10-09 13:01:59 +0200

    Pliki do uaktualnienia 13:02

--- teksty.py
+++ teksty.py
@@ -4,7 +4,7 @@ str2 = f'wiek = {wiek}\n'
 # \t tabulator \n następny wiersz
 str3 = f"""
 ******************************************
-*3 to jest tekst 3,
+*3 to jest tekst 3, to też wiek {wiek}
  *                   ale ma specjalne opcje 
 *i kolejne wiersze
 ******************************************"""
