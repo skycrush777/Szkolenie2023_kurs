c24849849da6e3efe80be07ca6a239e58696cb1d
skycrush777 <skrajewski@zut.edu.pl>
2023-10-12 11:37:48 +0200

    Pliki do uaktualnienia 11:37

--- z_ftp.py
+++ z_ftp.py
@@ -4,4 +4,10 @@ ftp = ftplib.FTP('test.rebex.net')
 logowanie = ftp.login('demo', 'password')
 
 print(logowanie)
-katalog =
+katalog = ftp.nlst()
+print(katalog)
+plik = ftp.retrbinary('RETR readme.txt',open('plik_z_ftp','wb').write)
+print(plik)
+katalog2 = ftp.nlst('pub/example')
+print(katalog2)
+plik2 = ftp.retrbinary('RETR pub/example/KeyGenerator.png', open('grafika.png','wb').write)
