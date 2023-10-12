9b217fc054c36101601523b1f9dc348f7559c518
skycrush777 <skrajewski@zut.edu.pl>
2023-10-12 10:37:29 +0200

    Pliki do uaktualnienia 10:37

--- z_xml.py
+++ z_xml.py
@@ -5,10 +5,23 @@ import requests
 url = requests.get('https://api.weatherapi.com/v1/current.xml?key=d863af85e8fa43618f682810220812&q=Lodz&aqi=no')
 print(url.status_code)
 plik = url.text
+
 with open('danexml.xml', 'w') as f:
     f.write(plik)
 
 with open('danexml.xml','r') as f:
     tree = ET.parse(f)
 
-print(tree)
+root = tree.getroot()
+print(root.tag)
+print(root.attrib)
+for dziecko in root:
+    print(dziecko.tag, dziecko.attrib)
+    # sprawdzamy gałęzie drzewa dziecko w root xml
+
+x=[elem.tag for elem in root.iter()]
+for i in x:
+    print(i)
+
+for miasto in root.iter('name'):
+    print(miasto.text)
