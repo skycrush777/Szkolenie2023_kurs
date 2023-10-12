029ae02a5eb780d74f878b6b46d4551bcdc0bfd5
skycrush777 <skrajewski@zut.edu.pl>
2023-10-12 12:56:38 +0200

    Pliki do uaktualnienia 12:56

--- wyr_reg.py
+++ wyr_reg.py
@@ -1,8 +1,21 @@
 import re
 
 tekst = "Python to nie tylko gad, ale także język programowania"
-wynik = re.match(r'Python', tekst)
+# wynik = re.match(r'Python', tekst) wykonuje szykanie od początku
+wzorzec = re.compile(r'ni')
+# wynik = wzorzec.search(tekst)
+wynik = wzorzec.findall(tekst)
+print(type(wynik))
+
+
 print(wynik)
 
-print(f'Dopasowano: {wynik.group()}, poczatek: {wynik.span()[0]}, koniec: {wynik.span()[1]}')
-print(f'Dopasowano: {wynik.group()}, poczatek: {wynik.start()}, koniec: {wynik.end()}')
+# print(f'Dopasowano: {wynik.group()}, poczatek: {wynik.span()[0]}, koniec: {wynik.span()[1]}')
+# if wynik:
+    # print(f'Dopasowano: {wynik.group()}, poczatek: {wynik.start()}, koniec: {wynik.end()}')
+
+
+if len(wynik)>0:
+    print(f'Dopasowano: {wynik}')
+else:
+    print(wynik)
